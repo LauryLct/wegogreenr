b718b379a47fbb100e427fcc7bc9e2a436cc27fb
stephane VINCENT <stephane.vincent0@gmail.com>
2019-06-05 11:57:41 +0200

    profile

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,51 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_05_082400) do
+ActiveRecord::Schema.define(version: 2019_06_05_095706) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "activities", force: :cascade do |t|
+    t.string "category"
+    t.string "name"
+    t.text "description"
+    t.string "street"
+    t.string "zipcode"
+    t.string "city"
+    t.float "latitude"
+    t.float "longitude"
+    t.date "starting_date"
+    t.date "ending_date"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_activities_on_user_id"
+  end
+
+  create_table "messages", force: :cascade do |t|
+    t.text "content"
+    t.bigint "user_id"
+    t.bigint "activity_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["activity_id"], name: "index_messages_on_activity_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "profiles", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "nickname"
+    t.string "gender"
+    t.integer "age"
+    t.string "description"
+    t.string "photo"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +68,8 @@ ActiveRecord::Schema.define(version: 2019_06_05_082400) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "activities", "users"
+  add_foreign_key "messages", "activities"
+  add_foreign_key "messages", "users"
+  add_foreign_key "profiles", "users"
 end
