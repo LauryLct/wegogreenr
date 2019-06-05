2e6b05b1ec3a1e763a327c26ab89cd66fc3340c5
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-06-05 13:10:24 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroy_all"
+User.destroy_all
+Activity.destroy_all
+
+
+Daniel = User.new(
+  email = "daniel@daniel.com",
+  password = "azerty",
+  first_name = "Daniel",
+  last_name = "Feline",
+  photo = "",
+  age = 44
+)
+
+Laury = User.new(
+  email = "laury@laury.com",
+  password = "azerty",
+  first_name = "Laury",
+  last_name = "Lecaut",
+  photo = "",
+  age = 32
+
+)
+
+Stephane = User.new(
+  email = "steph@steph.com",
+  password = "azerty",
+  first_name = "Stephane",
+  last_name = "Vincent",
+  photo = "",
+  age = 36
+
+)
+
+Marie = User.new(
+  email = "marie@marie.com",
+  password = "azerty",
+  first_name = "Marie",
+  last_name = "Vincent",
+  photo = "",
+  age = 32
+
+)
