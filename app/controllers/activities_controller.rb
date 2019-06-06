149e952dc31adc21b0cfabddabb99b2af5648e09
Daniel Feline <daniel.feline@yahoo.com>
2019-06-06 14:06:55 +0200

    map in the box

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,5 +1,14 @@
 class ActivitiesController < ApplicationController
   def index
+    @activities = Activity.where.not(latitude: nil, longitude: nil)
+
+    @markers = @activities.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude,
+        image_url: helpers.asset_url('Logo3-simple.png')
+      }
+    end
   end
 
   def show
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,4 +1,11 @@
 class Activity < ApplicationRecord
   belongs_to :user
   has_many :messages
+  geocoded_by :address
+
+  def address
+    [street, zipcode, city].compact.join(', ')
+  end
+  mount_uploader :photo, PhotoUploader
+  after_validation :geocode
 end
