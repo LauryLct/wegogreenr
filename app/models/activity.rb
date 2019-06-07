0607c6c7b3ecfae9c85a1d91d4c224f732bdb15f
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-06-07 13:46:17 +0200

    review

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,7 @@ class Activity < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   has_many :messages, dependent: :destroy
+  has_many :reviews, dependent: :destroy
   validates :category, inclusion: { in: ["Shop Eco-friendly", "Alimentation", "Restaurant Bio", "Atelier", "Event"] }
 
 
