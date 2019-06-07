1750a17a2351999db5d08c153b3459666653a0b8
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-06-07 13:11:40 +0200

    model activity

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,11 +2,14 @@ class Activity < ApplicationRecord
   belongs_to :user
   geocoded_by :address
   has_many :messages, dependent: :destroy
+  validates :category, inclusion: { in: ["Shop Eco-friendly", "Alimentation", "Restaurant Bio", "Atelier", "Event"] }
+
 
   def address
     [street, zipcode, city].compact.join(', ')
   end
 
+  
   mount_uploader :photo, PhotoUploader
   after_validation :geocode
 end
