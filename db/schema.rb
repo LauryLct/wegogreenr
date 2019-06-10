1e389024022667b29e96161a8e7590eeca2809e7
mariepierre vincent <mariepierre.vincent.sch@gmail.com>
2019-06-10 17:35:45 +0200

    ras

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_10_113028) do
+ActiveRecord::Schema.define(version: 2019_06_10_133335) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,15 @@ ActiveRecord::Schema.define(version: 2019_06_10_113028) do
     t.index ["user_id"], name: "index_activities_on_user_id"
   end
 
+  create_table "hearts", force: :cascade do |t|
+    t.bigint "post_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_hearts_on_post_id"
+    t.index ["user_id"], name: "index_hearts_on_user_id"
+  end
+
   create_table "messages", force: :cascade do |t|
     t.text "content"
     t.bigint "user_id"
@@ -41,6 +50,11 @@ ActiveRecord::Schema.define(version: 2019_06_10_113028) do
     t.datetime "updated_at", null: false
     t.index ["activity_id"], name: "index_messages_on_activity_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "profiles", force: :cascade do |t|
