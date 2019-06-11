d1a88a3a7e44abe783a9580d5c14a967c0936a67
stephane VINCENT <stephane.vincent0@gmail.com>
2019-06-11 11:57:32 +0200

    champs ok mais pas de css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
 
+  before_action :check_profile!, if: :user_signed_in?
+
   def after_sign_in_path_for(resource)
     session[:current_path] || activities_path
+  end
+
+  private
+
+  def check_profile!
+    flash.now[:alert] = "Veuillez renseigner un pseudo pour continuer"
+    redirect_to new_profile_path unless current_user.profile.present? 
   end
 end
--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,19 +1,41 @@
 class ProfilesController < ApplicationController
-  def index
-  end
-
-  def show
-  end
+  skip_before_action :check_profile!, only: [:new, :create]
 
   def new
+    @user = current_user
+    @profile = Profile.new
   end
-
+  
   def create
+    @profile = Profile.new(profile_params)
+    @profile.user = current_user
+    if @profile.save
+      redirect_to root_path
+      flash[:success] = "Profil mis à jour"
+    else
+      render :new
+      flash.now[:alert] = "Une erreur est survenue"
+    end
   end
 
   def edit
+    @profile.user = current_user
   end
-
+  
   def update
+    @profile.user = current_user
+    if @profile.update(profile_params)
+      redirect_to root_path
+      flash[:alert] = "Profil mis à jour"
+    else
+      render :edit
+      flash[:alert].now = "Une erreur est survenue"
+    end
+  end
+  
+  private
+  
+  def profile_params
+    params.require(:profile).permit(:first_name, :last_name, :nickname, :gender, :age, :description, :photo)
   end
 end
