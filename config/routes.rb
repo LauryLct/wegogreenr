d18daf984ee22a3e9c6772f442f0631ddbcd3b88
stephane VINCENT <stephane.vincent0@gmail.com>
2019-06-12 17:03:36 +0200

    ajout en cours

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   resources :hearts
   mount ActionCable.server => "/cable"
   resources :activities do
@@ -12,6 +13,8 @@ Rails.application.routes.draw do
   resources :profiles, only: [:new, :create, :edit, :update]
 
   devise_for :users
+
+  get '/users/dashboard', to: 'users#show', as: 'dashboard'
 
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
